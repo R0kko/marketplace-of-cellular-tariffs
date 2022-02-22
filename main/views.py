e07ffb18f8a204003366d4ff649ee30de2a94f82
Dezhin Dmitriy <dadezhin@edu.hse.ru>
2022-02-22 13:49:21 +0300

    added index page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index_page(request):
+    context = {}
+
+    return render(request, 'index.html', context)
--- marketplace_of_ct/settings.py
+++ marketplace_of_ct/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'marketplace_of_ct.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'main/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
